0f0e1f1d86318e791d1d7ce56652eff8d0ecb939
ziad <ziad.nouh111@gmail.com>
2024-10-06 23:58:59 +0300

    chore:  use track routes in entry point

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ const requestLogger = require("./middlewares/requestLogger");
 const cookieParser = require("cookie-parser");
 
 const userRoutes = require("./routes/user.routes");
+const trackRoutes = require("./routes/track.routes");
 
 const UserController = require("./controllers/user.controller");
+const TrackController = require("./controllers/track.controller");
 
 const UserRepository = require("./repositories/user.repository");
+const TrackRepository = require("./repositories/track.repository");
 
 const userRepository = new UserRepository();
+const trackRepository = new TrackRepository();
 
 const userController = new UserController(userRepository);
+const trackController = new TrackController(trackRepository);
 
 const app = express();
 
@@ -36,6 +41,7 @@ app.use(requestLogger);
 app.use(morgan("short"));
 
 mainRouter.use("/users", userRoutes(userController));
+mainRouter.use("/tracks", trackRoutes(trackController));
 
 app.use("/api/v1", mainRouter);
 
