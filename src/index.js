e95d060882790f8a840a31ca4e57fb0766b2b73a
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-10-08 22:06:49 +0200

    chore: Add validation and permissions for branch operations

--- src/index.js
+++ src/index.js
@@ -15,24 +15,29 @@ const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const adminRoutes = require("./routes/admin.routes");
 const trackRoutes = require("./routes/track.routes");
+const branchRoutes = require("./routes/branch.routes");
 
 const UserController = require("./controllers/user.controller");
 const AuthController = require("./controllers/auth.controller");
 const AdminController = require("./controllers/admin.controller");
 const TrackController = require("./controllers/track.controller");
+const BranchController = require("./controllers/branch.controller");
 
 const UserRepository = require("./repositories/user.repository");
 const AdminRepository = require("./repositories/admin.repository");
 const TrackRepository = require("./repositories/track.repository");
+const BranchRepository = require("./repositories/branch.repository");
 
 const userRepository = new UserRepository();
 const adminRepository = new AdminRepository();
 const trackRepository = new TrackRepository();
+const branchRepository = new BranchRepository();
 
 const userController = new UserController(userRepository);
 const authController = new AuthController(adminRepository);
 const adminController = new AdminController(adminRepository);
 const trackController = new TrackController(trackRepository);
+const branchController = new BranchController(branchRepository);
 
 const app = express();
 
@@ -53,12 +58,8 @@ app.use(morgan("short"));
 mainRouter.use("/users", userRoutes(userController));
 mainRouter.use("/auth", authRoutes(authController));
 mainRouter.use("/tracks", trackRoutes(trackController));
-mainRouter.use(
-  "/admins",
-  auth,
-  checkRole(["super admin"]),
-  adminRoutes(adminController)
-);
+mainRouter.use("/admins", auth, checkRole(["super admin"]), adminRoutes(adminController));
+mainRouter.use("/branches", branchRoutes(branchController));
 
 app.use("/api/v1", mainRouter);
 
