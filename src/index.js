1af7752513f10c97c592a7b1dbe454a02f78b217
AHmedEMadEG <amoemo141996@gmail.com>
2024-10-10 20:59:01 +0300

    fix: removed user resource

--- src/index.js
+++ src/index.js
@@ -11,43 +11,31 @@ const cookieParser = require("cookie-parser");
 const auth = require("./middlewares/auth");
 const checkRole = require("./middlewares/checkRole");
 
-const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const adminRoutes = require("./routes/admin.routes");
 const trackRoutes = require("./routes/track.routes");
-
 const branchRoutes = require("./routes/branch.routes");
 const graduateRoutes = require("./routes/graduate.routes");
 
-const UserController = require("./controllers/user.controller");
 const AuthController = require("./controllers/auth.controller");
 const AdminController = require("./controllers/admin.controller");
 const TrackController = require("./controllers/track.controller");
-
 const BranchController = require("./controllers/branch.controller");
 const GraduateController = require("./controllers/graduate.controller");
 
-
-const UserRepository = require("./repositories/user.repository");
 const AdminRepository = require("./repositories/admin.repository");
 const TrackRepository = require("./repositories/track.repository");
-
 const BranchRepository = require("./repositories/branch.repository");
 const GraduateRepository = require("./repositories/graduate.repository");
 
-const userRepository = new UserRepository();
 const adminRepository = new AdminRepository();
 const trackRepository = new TrackRepository();
-
 const branchRepository = new BranchRepository();
-
 const graduateRepository = new GraduateRepository();
 
-const userController = new UserController(userRepository);
 const authController = new AuthController(adminRepository);
 const adminController = new AdminController(adminRepository);
 const trackController = new TrackController(trackRepository);
-
 const branchController = new BranchController(branchRepository);
 const graduateController = new GraduateController(graduateRepository);
 
@@ -67,13 +55,15 @@ app.use(cookieParser());
 app.use(requestLogger);
 app.use(morgan("short"));
 
-mainRouter.use("/users", userRoutes(userController));
 mainRouter.use("/auth", authRoutes(authController));
 mainRouter.use("/tracks", trackRoutes(trackController));
-
-mainRouter.use("/admins", auth, checkRole(["super admin"]), adminRoutes(adminController));
+mainRouter.use(
+  "/admins",
+  auth,
+  checkRole(["super admin"]),
+  adminRoutes(adminController)
+);
 mainRouter.use("/branches", branchRoutes(branchController));
-
 mainRouter.use("/grads", graduateRoutes(graduateController));
 
 app.use("/api/v1", mainRouter);
