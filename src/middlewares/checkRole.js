69b828cde9a88d81c85f5cfbea60d06558430723
AHmedEMadEG <amoemo141996@gmail.com>
2024-10-13 14:35:36 +0300

    fix: restuructured the error response in checkRole middleware

--- src/middlewares/checkRole.js
+++ src/middlewares/checkRole.js
@@ -1,8 +1,7 @@
-
 const checkRole = (roles) => async (req, res, next) => {
-    const { role } = req.admin;
-    if (role && roles.includes(role)) return next();
-    res.status(403).send("Not authorized");
+  const { role } = req.admin;
+  if (role && roles.includes(role)) return next();
+  res.status(403).send({ error: "Unauthorized" });
 };
 
 module.exports = checkRole;
